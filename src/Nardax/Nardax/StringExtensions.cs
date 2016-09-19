596813e9790dbb0a60d19cf35f92bcdc6de8f5ab
pk <pk94942327@gmail.com>
2016-09-19 22:08:00 +0200

    Lagt till metoden RemoveWhteChars()

--- src/Nardax/Nardax/StringExtensions.cs
+++ src/Nardax/Nardax/StringExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Text.RegularExpressions;
 
 namespace Nardax
 {
@@ -26,6 +27,11 @@ namespace Nardax
             }
 
             return value.Substring(0, maxLength);
+        }
+
+        public static string RemoveWhiteChars(this string value)
+        {
+            return Regex.Replace(value, "\\s", "");
         }
     }
 }
