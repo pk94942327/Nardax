608b6f4a92c52b1611af573662b576a456aa64ef
pk <pk94942327@gmail.com>
2016-09-19 19:00:55 +0200

    Rättningar

--- src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
+++ src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
@@ -54,44 +54,4 @@ namespace Nardax.Tests
         }
     }
 }
-        [TestMethod]
-        public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateLeft(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateLeft_ValueIsLarger_TruncateRemovesLeftSide_Kalle_Kula_Testar()
-        {
-            var expected = "789";
-
-            var result = _value.TruncateLeft(1);
-
-            Assert.AreEqual(expected, result);
-        }
-    }
-}
+  
