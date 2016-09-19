a0fc4729525bf9ed96817390bba3ae579c990417
pk <pk94942327@gmail.com>
2016-09-19 18:15:24 +0200

    Go banana - raderat i TruncateStringExtensionsTests.cs

--- src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
+++ src/Nardax.UnitTests/TruncateStringExtensionsTests.cs
@@ -24,34 +24,3 @@ namespace Nardax.Tests
             Assert.AreEqual(expected, result);
         }
 
-        [TestMethod]
-        public void TruncateLeft_ValueIsSmaller_TruncateRemovesLeftSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateLeft(20);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsLarger_TruncateRemovesRightSide()
-        {
-            var expected = "1";
-
-            var result = _value.TruncateRight(1);
-
-            Assert.AreEqual(expected, result);
-        }
-
-        [TestMethod]
-        public void TruncateRight_ValueIsSmaller_TruncateRemovesRightSide()
-        {
-            var expected = "123456789";
-
-            var result = _value.TruncateRight(20);
-
-            Assert.AreEqual(expected, result);
-        }
-    }
-}
